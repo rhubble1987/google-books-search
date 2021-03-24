c627050a73d0361ccee0cbd7421bad831505f8a5
Ryan Hubble <rhubble1987@gmail.com>
2021-03-23 23:47:58 -0400

    deploying

--- client/src/components/SavedBook.js
+++ client/src/components/SavedBook.js
@@ -1,19 +1,19 @@
 function SavedBook(props) {
     return (
-      <div className="card" data={props.id}>
+      <div className="card">
         <div className="card-header">
-          <p className="card-header-title title is-4">{props.title}</p>
+          <p className="card-header-title title is-4">{props.book.title}</p>
         </div>
         <div className="card-content">
           <div className="media-content">
             <div className="columns">
               <div className="column is-four-fifths">
-                <p className="mb-3">Written by:{props.authors}</p>
+                <p className="mb-3">Written by:{props.book.authors}</p>
               </div>
               <div className="column">
                 <div className="field is-grouped is-flex is-justify-content-flex-end">
                   <div className="control">
-                    <a className="button is-info" href={props.link}>View Info</a>
+                    <a className="button is-info" target="_blank" href={props.book.link}>View Info</a>
                   </div>
                   <div className="control">
                     <a className="button is-info">Delete</a>
@@ -28,7 +28,7 @@ function SavedBook(props) {
                   <div className="media-left">
                     <figure className="image">
                       <img
-                        src="https://placekitten.com/200/300"
+                        src={props.book.image}
                         alt="Placeholder image"
                       />
                     </figure>
@@ -36,7 +36,7 @@ function SavedBook(props) {
                 </div>
               </div>
               <div className="column">
-                <p>{props.description}</p>
+                <p>{props.book.description}</p>
               </div>
             </div>
           </div>
