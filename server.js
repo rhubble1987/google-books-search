0070bfa1f936636815ce59fca0568c263917f9ea
Ryan Hubble <rhubble1987@gmail.com>
2021-03-23 23:12:01 -0400

    deploying

--- server.js
+++ server.js
@@ -15,7 +15,12 @@ if (process.env.NODE_ENV === "production") {
 }
 
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", { 
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+ });
 
 
 app.post("/api/books", (req,res) => {
