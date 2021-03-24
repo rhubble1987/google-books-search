b78b7cb924f5980e148ef5486e3a9f3fd1ab16a0
Ryan Hubble <rhubble1987@gmail.com>
2021-03-24 08:59:44 -0400

    fix for API calls

--- client/utils/API.js
+++ client/utils/API.js
@@ -5,13 +5,13 @@ export default {
         return axios.get(`https://www.googleapis.com/books/v1/volumes?q=${query}&key=AIzaSyAE-eBEmJDu_9gFhmV0YWUFk_gCEWUYZQE`);
     },
     loadSavedBooks: function() {
-        return axios.get('http://localhost:3000/api/books');
+        return axios.get('/api/books');
     },
     deleteSavedBook: function(bookId) {
-        return axios.delete(`http://localhost:3000/api/books/${bookId}`);
+        return axios.delete(`/api/books/${bookId}`);
     },
     saveBook: function(book) {
-        return axios.post('http://localhost:3000/api/books', {
+        return axios.post('/api/books', {
             title: book.volumeInfo.title,
             authors: book.volumeInfo.authors,
             description: book.volumeInfo.description,
